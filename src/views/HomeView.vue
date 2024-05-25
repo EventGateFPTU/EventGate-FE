e367bddfbfcdab23b266b9a68cf4bc1a749c3997
ezarp <chien.nguyen@techlabcorp.local>
2024-05-26 01:34:18 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Button from 'primevue/button'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="flex h-screen items-center justify-center">
+    <Button @click="count++"> Count is: {{ count }} </Button>
+  </div>
+</template>
